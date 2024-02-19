6802f4ef1c8d5fdf352ac715954a7ca30e1cf6b6
KalebP <pelletierk3@wit.edu>
2024-02-19 15:45:22 -0500

    Add project files.

---
+++ OpenGamesApp/Form1.Designer.cs
@@ -0,0 +1,144 @@
+﻿namespace OpenGamesApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Open = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // Open
+            // 
+            this.Open.BackColor = System.Drawing.Color.Silver;
+            this.Open.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F);
+            this.Open.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.Open.Location = new System.Drawing.Point(12, 282);
+            this.Open.Name = "Open";
+            this.Open.Size = new System.Drawing.Size(297, 93);
+            this.Open.TabIndex = 0;
+            this.Open.Text = "OPEN NOW!!!";
+            this.Open.UseVisualStyleBackColor = false;
+            this.Open.Click += new System.EventHandler(this.OpenRL_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.BackColor = System.Drawing.Color.Black;
+            this.listBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.listBox1.ForeColor = System.Drawing.Color.Lime;
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 20;
+            this.listBox1.Location = new System.Drawing.Point(12, 12);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(297, 264);
+            this.listBox1.TabIndex = 1;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(330, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(512, 264);
+            this.pictureBox1.TabIndex = 2;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F);
+            this.label1.ForeColor = System.Drawing.Color.Silver;
+            this.label1.Location = new System.Drawing.Point(327, 282);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(64, 25);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "label1";
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.Silver;
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F);
+            this.button1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.button1.Location = new System.Drawing.Point(719, 282);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(147, 93);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Press me!";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.Silver;
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F);
+            this.button2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.button2.Location = new System.Drawing.Point(652, 339);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(61, 36);
+            this.button2.TabIndex = 6;
+            this.button2.Text = "reset?";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Gray;
+            this.ClientSize = new System.Drawing.Size(878, 387);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.Open);
+            this.KeyPreview = true;
+            this.Name = "Form1";
+            this.Text = "OpenGamesV1";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_closing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.form_key);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button Open;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ OpenGamesApp/Form1.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Diagnostics;
+using System.ComponentModel;
+using System.Security.Cryptography;
+using System.IO;
+
+namespace OpenGamesApp
+{
+    public partial class Form1 : Form
+    {
+        int click_counter = 0;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void OpenRL_Click(object sender, EventArgs e)
+        {
+            string item = listBox1.SelectedItem.ToString(); 
+            
+            Process.Start("c:\\Users\\kpell\\OneDrive\\Desktop\\Games\\"+item+".url");
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            label1.Text = DateTime.Now.ToString("hh:mm tt dddd, MMM dd");
+            //ADD QUOTES
+
+            string count_string = File.ReadAllText("Counter.txt");
+            button1.Text = "Press me!\n"+count_string;
+            click_counter = Convert.ToInt16(count_string);
+
+
+            pictureBox1.Image = Image.FromFile("giphy.gif");
+            pictureBox1.Show();
+            string desktopPath = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
+            
+            FileInfo fileInfo = new FileInfo(desktopPath + @"\Games");
+
+            if (!fileInfo.Exists)
+                Directory.CreateDirectory(fileInfo.Directory.FullName);
+
+
+
+            string path = desktopPath + @"\\Games";
+            int count = path.Length;
+            string[] files = System.IO.Directory.GetFiles(desktopPath + @"\Games");
+            foreach(string file in files)
+            {
+                string name = file.Substring(count);
+                string[] nameSplit = name.Split('.');
+                listBox1.Items.Add(nameSplit[0]);
+            }
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            click_counter = click_counter + 1;
+            button1.Text = "Press me!\n" + click_counter.ToString();
+        }
+
+        private void Form1_closing(object sender, FormClosingEventArgs e)
+        {
+            File.WriteAllText("Counter.txt", click_counter.ToString());
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            MessageBoxButtons buttonMessage = MessageBoxButtons.YesNo;
+            DialogResult result = MessageBox.Show("You weally wanna reset? :(", "DONT DO IT PLEASE", buttonMessage);
+            if (result == DialogResult.Yes)
+            {
+                click_counter = 0;
+                button1.Text = "Press me!\n" + click_counter.ToString();
+            }
+            else
+            {
+                
+            }
+        }
+
+
+
+        private void form_key(object sender, KeyEventArgs e)
+        {
+            if (e.KeyValue == ' ')
+            {
+                e.Handled = true;
+                button1.PerformClick();
+            }
+        }
+    }
+}
